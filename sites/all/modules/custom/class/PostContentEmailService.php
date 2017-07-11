c5e2452e5db31c3d0c275e9a25510321b94df408
Noah-Dev <ark_code@protonmail.com>
2017-07-10 19:46:25 -0500

    Working code - need to implement email feature

--- sites/all/modules/custom/class/PostContentEmailService.php
+++ sites/all/modules/custom/class/PostContentEmailService.php
@@ -11,35 +11,44 @@ class PostContentEmailService
 
   public function run()
   {
-
-    $sql_query = "SELECT     
+    $sql_query = "SELECT
                     n.nid,
-                    n.uid,
-                    n.changed
-                  FROM ccf_node n 
-                  WHERE 
+                    n.uid
+                  FROM ccf_node n
+                  WHERE
                     n.type = 'project' AND
                     n.status = 1 AND
                     n.changed <= UNIX_TIMESTAMP( DATE_ADD(DATE_ADD(CURDATE(), INTERVAL -6 MONTH), INTERVAL -1 SECOND) );
                   ";
-    
-    //Using above query, load the projects
-    $projectNodeIds = array_keys(db_query($sql_query)->fetchAllKeyed());
-		$projects = node_load_multiple($projectNodeIds);
-    //Go through each project to 1) Send email to owner and 2) Set the timestamp to now
 
-    //Trying to get a field from node
-    foreach ($projects as &$project){
-      $some_variable = $project->field_project_type[$project->language][0]['value'];
+    $queryResults = db_query($sql_query)->fetchAll();
+    if (empty($queryResults))
+      return;
+
+    $length = count($queryResults);
+    $projectNodeIds = array_fill(0, $queryResults, null);
+    $projectUserIds = array_fill(0, $queryResults, null);
+
+    for ($i = 0; $i < $length; $i++) {
+      $projectId = intval($queryResults[$i]->nid);
+      $projectNodeIds[$i] = $projectId;
+      $projectUserIds[$projectId] = intval($queryResults[$i]->uid);
     }
-    //Watchdog - used for testing 
-    //watchdog([module name], [message string], [array of token replacements], [severity constant]);
-    watchdog(     
-      'custom',
-      'Result: !some',
-      array('!some'=>$some_variable),
-      WATCHDOG_DEBUG 
-    );
+
+    $users = user_load_multiple($projectUserIds);
+    $projects = node_load_multiple($projectNodeIds);
+
+
+    foreach ($projects as $project) {
+      $user = $users[(int)$project->uid];
+      $email = $user->mail;
+      $fullname = $user->field_full_name['und'][0]['value'];
+      $project_name = $project->title;
+      $last_rev = $project->revision_timestamp;
+      $test = 0;
+
+      // .. do code here
+    }
   }
 
 }
